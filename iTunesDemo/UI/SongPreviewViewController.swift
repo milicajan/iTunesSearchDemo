46f8bde9da25e5ad92b649fb97af8cd6a21ee2a7
Milica Jankovic <milica.jankovic@pbssolutions.net>
2020-08-22 16:26:08 +0200

    added SongListVC and SongPreviewVC

---
+++ iTunesDemo/UI/SongPreviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SongPreviewViewController.swift
+//  iTunesDemo
+//
+//  Created by Milica Jankovic on 22/08/2020.
+//  Copyright © 2020 Milica Jankovic. All rights reserved.
+//
+
+import UIKit
+
+class SongPreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
